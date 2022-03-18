66cb4cb4237884f8e3988cb7255aa0bff4b5dcdb
Riku Rainio <riku.rainio@tuni.fi>
2022-03-18 22:07:11 +0200

    add files to repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Punklist from './components/Punklist';
+import { Main } from './components/Main';
 
 function App() {
+  const [punkListData, setPunkListData] = useState([])
+  const [selectedPunk, setSelectedPunk] = useState(0)
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await
+          axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x717263e8ad517b4bd77a28e1d70F7Aa39EcdD6f8&order_direction=asc')
+
+      console.log(openseaData.data.assets)
+      setPunkListData(openseaData.data.assets)
+    }
+
+    return getMyNfts()
+  }, [])
+
+  let reverseArray = [...punkListData].reverse()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Header />
+
+      {reverseArray.length > 0 && (
+        <>
+          <Main punkListData={reverseArray} selectedPunk={selectedPunk}/>
+          <Punklist punkListData={reverseArray} setSelectedPunk={setSelectedPunk}/>
+        </>
+      )}
+
     </div>
-  );
+  )
 }
 
 export default App;
